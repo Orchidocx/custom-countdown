e5b8ca8cc115f5ef35c5ba0fd5a08e134d4721a2
Jacky Li <ilikemacaronii@gmail.com>
2020-09-03 14:23:06 -0700

    finish functionality for countdown; no local storage ability

--- script.js
+++ script.js
@@ -1,5 +1,85 @@
 const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+let countdownTitle = '';
+let countdownDate = '';
+let countdownValue = Date;
+let coundtownActive;
+
+const completeEl = document.getElementById('complete');
+const completeElInfo = document.getElementById('complete-info');
+const completeBtn = document.getElementById('complete-button');
+
+// In miliseconds
+const second = 1000; 
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+
+// Populate Countdown / Complete UI
+function updateDOM() {
+  coundtownActive = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
+    const days = Math.floor(distance/day);
+    const hours = Math.floor((distance % day) / hour);
+    const minutes = Math.floor((distance % hour) / minute);
+    const seconds = Math.floor((distance % minute) / second);
+
+    inputContainer.hidden = true;
+    if(distance < 0) {
+      countdownEl.hidden = true;
+      clearInterval(coundtownActive);
+      completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`;
+      completeEl.hidden = false;
+    } else {
+      // show countdown in progress
+      countdownElTitle.textContent = `${countdownTitle}`;
+      timeElements[0].textContent = `${days}`;
+      timeElements[1].textContent = `${hours}`;
+      timeElements[2].textContent = `${minutes}`;
+      timeElements[3].textContent = `${seconds}`;
+      completeEl.hidden = true;
+      countdownEl.hidden = false;
+    }
+  }, second);
+  
+}
+
+// Take Values from form input
+function updateCountdown(evt) {
+  evt.preventDefault();
+  countdownTitle = evt.srcElement[0].value;
+  countdownDate = evt.srcElement[1].value;
+  
+  if(countdownDate === '') {
+    alert('Please select a date for the countdown.');
+  } else {
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
+  }
+  
+}
+
+// Reset all values
+function reset() {
+  countdownEl.hidden = true;
+  completeEl.hidden = true;
+  inputContainer.hidden = false;
+  clearInterval(coundtownActive);
+  countdownTitle = '';
+  countdownDate = '';
+}
+
+// Evt Listeners
+countdownForm.addEventListener('submit', updateCountdown);
+countdownBtn.addEventListener('click', reset);
+completeBtn.addEventListener('click', reset);
